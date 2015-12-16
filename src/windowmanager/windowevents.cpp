0e38672cb0f43a3f05e2d570df623edc74106fbe
Tamas <tamas.bates@tum.de>
2015-12-16 16:19:53 +0100

    Wrapper for GLFW Events This adds a wrapper class for the window and input events we get from GLFW. It allows anyone to subscribe to events generated by a specific window.

---
+++ src/windowmanager/windowevents.cpp
@@ -0,0 +1,345 @@
+#include "windowevents.hpp"
+
+namespace ar
+{
+
+WindowEvents::WindowEvents(GLFWwindow* window)
+{
+  // give GLFW a reference to ourself so we can get it in the callbacks
+  glfwSetWindowUserPointer(window, this);
+
+  // register our callbacks with GLFW
+  glfwSetMouseButtonCallback(window, glfw_mousebutton_callback);
+  glfwSetCursorEnterCallback(window, glfw_mouseenter_callback);
+  glfwSetCursorPosCallback(window, glfw_mousemove_callback);
+  glfwSetKeyCallback(window, glfw_keyboard_callback);
+  glfwSetCharCallback(window, glfw_char_callback);
+  glfwSetScrollCallback(window, glfw_scroll_callback);
+  glfwSetWindowSizeCallback(window, glfw_windowsize_callback);
+  glfwSetFramebufferSizeCallback(window, glfw_framebuffersize_callback);
+}
+
+void WindowEvents::NotifyMouseButtonCallback(int button, int action, int mods)
+{
+  for (const auto& cb : _mouseButtonCallbacks)
+  {
+    cb.second(button, action, mods);
+  }
+}
+
+void WindowEvents::NotifyMouseMoveCallback(double xpos, double ypos)
+{
+  for (const auto& cb : _mouseMoveCallbacks)
+  {
+    cb.second(xpos, ypos);
+  }
+}
+
+void WindowEvents::NotifyMouseEnterExitCallback(int entered)
+{
+  for (const auto& cb : _mouseEnterExitCallbacks)
+  {
+    cb.second(entered);
+  }
+}
+
+void WindowEvents::NotifyScrollCallback(double xoffset, double yoffset)
+{
+  for (const auto& cb : _scrollCallbacks)
+  {
+    cb.second(xoffset, yoffset);
+  }
+}
+
+void WindowEvents::NotifyKeyboardKeyCallback(int key, int scancode, int action, int mods)
+{
+  for (const auto& cb : _keyCallbacks)
+  {
+    cb.second(key, scancode, action, mods);
+  }
+}
+
+void WindowEvents::NotifyKeyboardCharCallback(unsigned int codepoint)
+{
+  for (const auto& cb : _charCallbacks)
+  {
+    cb.second(codepoint);
+  }
+}
+
+void WindowEvents::NotifyWindowSizeCallback(int width, int height)
+{
+  for (const auto& cb : _windowSizeCallbacks)
+  {
+    cb.second(width, height);
+  }
+}
+
+void WindowEvents::NotifyFrameBufferSizeCallback(int width, int height)
+{
+  for (const auto& cb : _frameBufferSizeCallbacks)
+  {
+    cb.second(width, height);
+  }
+}
+
+WindowEvents::event_handle WindowEvents::SubscribeEvent(Event e, std::function<void(int)> cb)
+{
+  event_handle eh = NextHandle();
+  switch (e)
+  {
+    case Event::MouseEnterExit:
+    {
+      _mouseEnterExitCallbacks.insert(std::make_pair(eh, cb));
+      break;
+    }
+    default:
+    {
+      throw UnknownWindowEventException("Event: " + (int)e);
+    }
+  }
+
+  return eh;
+}
+
+WindowEvents::event_handle WindowEvents::SubscribeEvent(Event e, std::function<void(int,int)> cb)
+{
+  event_handle eh = NextHandle();
+  switch (e)
+  {
+    case Event::WindowSizeChanged:
+    {
+      _windowSizeCallbacks.insert(std::make_pair(eh, cb));
+      break;
+    }
+    case Event::FrameBufferSizeChanged:
+    {
+      _frameBufferSizeCallbacks.insert(std::make_pair(eh, cb));
+      break;
+    }
+    default:
+    {
+      throw UnknownWindowEventException("Event: " + (int)e);
+    }
+  }
+
+  return eh;
+}
+
+WindowEvents::event_handle WindowEvents::SubscribeEvent(Event e, std::function<void(int,int,int)> cb)
+{
+  event_handle eh = NextHandle();
+  switch (e)
+  {
+    case Event::MouseButtonStateChanged:
+    {
+      _mouseButtonCallbacks.insert(std::make_pair(eh, cb));
+      break;
+    }
+    default:
+    {
+      throw UnknownWindowEventException("Event: " + (int)e);
+    }
+  }
+
+  return eh;
+}
+
+WindowEvents::event_handle WindowEvents::SubscribeEvent(Event e, std::function<void(int,int,int,int)> cb)
+{
+  event_handle eh = NextHandle();
+  switch (e)
+  {
+    case Event::KeyboardStateChanged:
+    {
+      _keyCallbacks.insert(std::make_pair(eh, cb));
+      break;
+    }
+    default:
+    {
+      throw UnknownWindowEventException("Event: " + (int)e);
+    }
+  }
+
+  return eh;
+}
+
+WindowEvents::event_handle WindowEvents::SubscribeEvent(Event e, std::function<void(double,double)> cb)
+{
+  event_handle eh = NextHandle();
+  switch (e)
+  {
+    case Event::MouseMove:
+    {
+      _mouseMoveCallbacks.insert(std::make_pair(eh, cb));
+      break;
+    }
+    case Event::Scroll:
+    {
+      _scrollCallbacks.insert(std::make_pair(eh, cb));
+    }
+    default:
+    {
+      throw UnknownWindowEventException("Event: " + (int)e);
+    }
+  }
+
+  return eh;
+}
+
+WindowEvents::event_handle WindowEvents::SubscribeEvent(Event e, std::function<void(unsigned int)> cb)
+{
+  event_handle eh = NextHandle();
+  switch (e)
+  {
+    case Event::CharacterInput:
+    {
+      _charCallbacks.insert(std::make_pair(eh, cb));
+    }
+    default:
+    {
+      throw UnknownWindowEventException("Event: " + (int)e);
+    }
+  }
+
+  return eh;
+}
+
+void WindowEvents::UnsubscribeEvent(Event e, event_handle eh)
+{
+  switch (e)
+  {
+    case Event::WindowSizeChanged:
+    {
+      _windowSizeCallbacks.erase(eh);
+      break;
+    }
+    case Event::FrameBufferSizeChanged:
+    {
+      _frameBufferSizeCallbacks.erase(eh);
+      break;
+    }
+    case Event::MouseButtonStateChanged:
+    {
+      _mouseButtonCallbacks.erase(eh);
+      break;
+    }
+    case Event::MouseEnterExit:
+    {
+      _mouseEnterExitCallbacks.erase(eh);
+      break;
+    }
+    case Event::MouseMove:
+    {
+      _mouseMoveCallbacks.erase(eh);
+      break;
+    }
+    case Event::KeyboardStateChanged:
+    {
+      _keyCallbacks.erase(eh);
+      break;
+    }
+    case Event::CharacterInput:
+    {
+      _charCallbacks.erase(eh);
+      break;
+    }
+    case Event::Scroll:
+    {
+      _scrollCallbacks.erase(eh);
+      break;
+    }
+    default:
+    {
+      throw UnknownWindowEventException("Event: " + (int)e);
+    }
+  }
+}
+
+void WindowEvents::glfw_mousebutton_callback( GLFWwindow *window, int button, int action, int mods )
+{
+  // get a 'this' reference
+  WindowEvents* winEvents = static_cast<WindowEvents*>(glfwGetWindowUserPointer(window));
+  if (winEvents != NULL)
+  {
+    winEvents->NotifyMouseButtonCallback(button, action, mods);
+  }
+}
+
+void WindowEvents::glfw_mousemove_callback( GLFWwindow *window, double xpos, double ypos )
+{
+  // get a 'this' reference
+  WindowEvents* winEvents = static_cast<WindowEvents*>(glfwGetWindowUserPointer(window));
+  if (winEvents != NULL)
+  {
+    winEvents->NotifyMouseMoveCallback(xpos, ypos);
+  }
+}
+
+void WindowEvents::glfw_mouseenter_callback( GLFWwindow *window, int entered )
+{
+  // get a 'this' reference
+  WindowEvents* winEvents = static_cast<WindowEvents*>(glfwGetWindowUserPointer(window));
+  if (winEvents != NULL)
+  {
+    winEvents->NotifyMouseEnterExitCallback(entered);
+  }
+}
+
+void WindowEvents::glfw_scroll_callback( GLFWwindow *window, double xoffset, double yoffset )
+{
+  // get a 'this' reference
+  WindowEvents* winEvents = static_cast<WindowEvents*>(glfwGetWindowUserPointer(window));
+  if (winEvents != NULL)
+  {
+    winEvents->NotifyScrollCallback(xoffset, yoffset);
+  }
+}
+
+void WindowEvents::glfw_keyboard_callback( GLFWwindow *window, int key, int scancode, int action, int mods )
+{
+  // get a 'this' reference
+  WindowEvents* winEvents = static_cast<WindowEvents*>(glfwGetWindowUserPointer(window));
+  if (winEvents != NULL)
+  {
+    winEvents->NotifyKeyboardKeyCallback(key, scancode, action, mods);
+  }
+}
+
+void WindowEvents::glfw_char_callback( GLFWwindow *window, unsigned int codepoint )
+{
+  // get a 'this' reference
+  WindowEvents* winEvents = static_cast<WindowEvents*>(glfwGetWindowUserPointer(window));
+  if (winEvents != NULL)
+  {
+    winEvents->NotifyKeyboardCharCallback(codepoint);
+  }
+}
+
+void WindowEvents::glfw_windowsize_callback( GLFWwindow *window, int width, int height )
+{
+  // get a 'this' reference
+  WindowEvents* winEvents = static_cast<WindowEvents*>(glfwGetWindowUserPointer(window));
+  if (winEvents != NULL)
+  {
+    winEvents->NotifyWindowSizeCallback(width, height);
+  }
+}
+
+void WindowEvents::glfw_framebuffersize_callback( GLFWwindow *window, int width, int height )
+{
+  // get a 'this' reference
+  WindowEvents* winEvents = static_cast<WindowEvents*>(glfwGetWindowUserPointer(window));
+  if (winEvents != NULL)
+  {
+    winEvents->NotifyFrameBufferSizeCallback(width, height);
+  }
+}
+
+WindowEvents::event_handle WindowEvents::NextHandle()
+{
+  static event_handle next = 0;
+  return ++next;
+}
+
+} // namespace ar
---
+++ src/windowmanager/windowevents.hpp
@@ -0,0 +1,103 @@
+#ifndef _WINDOWEVENTS_H
+#define _WINDOWEVENTS_H
+
+#ifndef GL_GLEXT_PROTOTYPES
+#define GL_GLEXT_PROTOTYPES
+#endif
+#include <GLFW/glfw3.h>
+
+#include <functional>
+#include <map>
+
+namespace ar
+{
+
+// Provides an abstraction between incoming window input events and
+// objects which need to consume them
+class WindowEvents
+{
+public:
+  typedef enum class
+  {
+    WindowSizeChanged,       // called when the user resizes the window
+    FrameBufferSizeChanged,  // called when GL framebuffer is changed (in code, or due to window size changing)
+
+    MouseButtonStateChanged, // called when any mouse button is pressed or released
+    MouseEnterExit,          // called when the mouse enters or exits the window
+    MouseMove,               // called any time the mouse moves over the window
+
+    KeyboardStateChanged,    // called any time a keyboard key is pressed or released
+    CharacterInput,          // called any time a printable character is received from the keyboard
+    Scroll,                  // called when the user uses the scroll wheel on their mouse (or scrolls from the keyboard)
+  } Event;
+
+  typedef unsigned int event_handle;
+
+  static const Event MouseButton        = Event::MouseButtonStateChanged;
+  static const Event MouseEnterExit     = Event::MouseEnterExit;
+  static const Event MouseMove          = Event::MouseMove;
+
+  static const Event WindowResized      = Event::WindowSizeChanged;
+  static const Event FrameBufferResized = Event::FrameBufferSizeChanged;
+
+  static const Event KeyboardKey        = Event::KeyboardStateChanged;
+  static const Event KeyboardChar       = Event::CharacterInput;
+  static const Event Scroll             = Event::Scroll;
+
+  WindowEvents(GLFWwindow* window);
+
+  // Registers a callback to be invoked when the given Event is detected
+  event_handle SubscribeEvent(Event e, std::function<void(int)> cb);
+  event_handle SubscribeEvent(Event e, std::function<void(int,int)> cb);
+  event_handle SubscribeEvent(Event e, std::function<void(int,int,int)> cb);
+  event_handle SubscribeEvent(Event e, std::function<void(int,int,int,int)> cb);
+  event_handle SubscribeEvent(Event e, std::function<void(double,double)> cb);
+  event_handle SubscribeEvent(Event e, std::function<void(unsigned int)> cb);
+
+  // Ensures a previously-subscribed even handler is not called again in the future
+  void UnsubscribeEvent(Event e, event_handle eh);
+
+  // Calls all subscribed handlers for the corresponding events
+  void NotifyMouseButtonCallback(int button, int action, int mods);
+  void NotifyMouseMoveCallback(double xpos, double ypos);
+  void NotifyMouseEnterExitCallback(int entered);
+  void NotifyScrollCallback(double xoffset, double yoffset);
+  void NotifyKeyboardKeyCallback(int key, int scancode, int action, int mods);
+  void NotifyKeyboardCharCallback(unsigned int codepoint);
+  void NotifyWindowSizeCallback(int width, int height);
+  void NotifyFrameBufferSizeCallback(int width, int height);
+
+  class UnknownWindowEventException : public std::runtime_error
+  {
+  public:
+    UnknownWindowEventException(const std::string& what_arg) : std::runtime_error(what_arg) {}
+  };
+
+private:
+  // callbacks we use to notify others when events happen
+  std::map<event_handle, std::function<void(int)> >             _mouseEnterExitCallbacks;
+  std::map<event_handle, std::function<void(int,int)> >         _windowSizeCallbacks;
+  std::map<event_handle, std::function<void(int,int)> >         _frameBufferSizeCallbacks;
+  std::map<event_handle, std::function<void(int,int,int)> >     _mouseButtonCallbacks;
+  std::map<event_handle, std::function<void(int,int,int,int)> > _keyCallbacks;
+  std::map<event_handle, std::function<void(double,double)> >   _mouseMoveCallbacks;
+  std::map<event_handle, std::function<void(double,double)> >   _scrollCallbacks;
+  std::map<event_handle, std::function<void(unsigned int)> >    _charCallbacks;
+
+  // callbacks registered with GLFW to notify us when events happen
+  static void glfw_mousebutton_callback( GLFWwindow *window, int button, int action, int mods );
+  static void glfw_mousemove_callback( GLFWwindow *window, double xpos, double ypos );
+  static void glfw_mouseenter_callback( GLFWwindow *window, int entered );
+  static void glfw_scroll_callback( GLFWwindow *window, double xoffset, double yoffset );
+  static void glfw_keyboard_callback( GLFWwindow *window, int key, int scancode, int action, int mods );
+  static void glfw_char_callback( GLFWwindow *window, unsigned int codepoint );
+  static void glfw_windowsize_callback( GLFWwindow *window, int width, int height );
+  static void glfw_framebuffersize_callback( GLFWwindow *window, int width, int height );
+
+  // gets a new unique handle to assign to an event
+  static event_handle NextHandle();
+};
+
+} // namespace ar
+
+#endif // _WINDOWEVENTS_H
