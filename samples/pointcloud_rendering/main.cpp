b65746c79977f5e38343f38e85b2ba4d91a01199
noirb <quetchl@gmail.com>
2016-03-30 20:38:50 +0200

    Update pointcloud_rendering sample to enumerate OpenNI devices & print some info about them

--- samples/pointcloud_rendering/main.cpp
+++ samples/pointcloud_rendering/main.cpp
@@ -24,7 +24,35 @@ void cloud_cb (const pcl::PointCloud<PointT>::ConstPtr& cloud)
 
 int main(void)
 {
-  // Create a pcl::Grabber to get data from sensor
+  // detect all OpenNI devices & print some info about them
+  openni_wrapper::OpenNIDriver::getInstance().updateDeviceList();
+  unsigned int nDevices = openni_wrapper::OpenNIDriver::getInstance().getNumberDevices();
+  if (nDevices == 0)
+  {
+    std::cout << "No compatible OpenNI devices detected! Exiting..." << std::endl;
+    return 0;
+  }
+
+  std::cout << nDevices << " OpenNI Devices Detected:" << std::endl;;
+  for (size_t i = 0; i < nDevices; i++)
+  {
+    auto device = openni_wrapper::OpenNIDriver::getInstance().getDeviceByIndex(i);
+    auto rgbOutputMode = device->getDefaultImageMode();
+    auto depthOutputMode = device->getDefaultDepthMode();
+
+    std::cout << std::endl;
+    std::cout << "\t" << "Device " << i << ": " << device->getVendorName() << " " << device->getProductName() << std::endl;
+    std::cout << "\t-------------------------------------------------------" << std::endl;
+
+    std::cout << "\t" << "Serial:" << device->getSerialNumber() << std::endl;
+    std::cout << "\t" << "Default Image Output Mode:" << std::endl;
+    std::cout << "\t\t" << "Width: " << rgbOutputMode.nXRes << "px, Height: " << rgbOutputMode.nYRes << "px, Frame Rate: " << rgbOutputMode.nFPS << " fps" << std::endl;
+    std::cout << "\t" << "Default Depth Output Mode:" << std::endl;
+    std::cout << "\t\t" << "Width: " << depthOutputMode.nXRes << "px, Height: " << depthOutputMode.nYRes << "px, Frame Rate: " << depthOutputMode.nFPS << " fps" << std::endl;
+  }
+
+  std::cout << "Opening first device..." << std::endl;
+  // Create a pcl::Grabber to get data from sensor (using first detected device by default)
   interface = new pcl::OpenNIGrabber("", pcl::OpenNIGrabber::OpenNI_QVGA_30Hz);
 
   // Create ar::ARVisualizer to send data to
